0a4ad1857e0b8158796aa9ec7dd47d1336578f5d
sumonsad <sumonsad22518@gmail.com>
2021-11-08 12:54:09 +0600

    second commit

--- functions.php
+++ functions.php
@@ -13,27 +13,36 @@ if(site_url()=="http://localhost/projecthasin"){
   }
   add_action("after_setup_theme","launcher_setup_theme");
   function launcher_assets(){
-      wp_enqueue_style("animate-css",get_theme_file_uri("/assets/css/animate.css"));
-      wp_enqueue_style("icomoon-css",get_theme_file_uri("/assets/css/icomoon.css"));
-      wp_enqueue_style("bootstrap-css",get_theme_file_uri("/assets/css/bootstrap.css"));
-      wp_enqueue_style("style-css",get_theme_file_uri("/assets/css/style.css"));
-      wp_enqueue_style("launcher", get_stylesheet_uri());
-
-      wp_enqueue_script("easing-jquery-js",get_theme_file_uri("/assets/js/jquery.easing.1.3.js"),array("jquery"),time(),true);
-      wp_enqueue_script("bootstrap-min-js",get_theme_file_uri("/assets/js/bootstrap.min.js"),array("jquery"),time(),true);
-      wp_enqueue_script("waypoints-min-js",get_theme_file_uri("/assets/js/jquery.waypoints.min.js"),array("jquery"),time(),true);
-      wp_enqueue_script("simplyCountdown-js",get_theme_file_uri("/assets/js/simplyCountdown.js"),array("jquery"),time(),true);
-      wp_enqueue_script("main-js",get_theme_file_uri("/assets/js/main.js"),array("jquery"),time(),true);
-
-      $launcher_year = get_post_meta(get_the_ID(),"year",true);
-      $launcher_month = get_post_meta(get_the_ID(),"month",true);
-      $launcher_day = get_post_meta(get_the_ID(),"day",true);
-
-      wp_localize_script("main-js","datedata",array(
-        "year" => $launcher_year,
-        "month" => $launcher_month,
-        "day" => $launcher_day
-      ));
+    if(is_page()){
+    $launcher_page_template= basename(get_page_template());
+    if($launcher_page_template=="launcher.php"){
+        wp_enqueue_style("animate-css",get_theme_file_uri("/assets/css/animate.css"));
+        wp_enqueue_style("icomoon-css",get_theme_file_uri("/assets/css/icomoon.css"));
+        wp_enqueue_style("bootstrap-css",get_theme_file_uri("/assets/css/bootstrap.css"));
+        wp_enqueue_style("style-css",get_theme_file_uri("/assets/css/style.css"));
+        wp_enqueue_style("launcher", get_stylesheet_uri());
+  
+        wp_enqueue_script("easing-jquery-js",get_theme_file_uri("/assets/js/jquery.easing.1.3.js"),array("jquery"),time(),true);
+        wp_enqueue_script("bootstrap-min-js",get_theme_file_uri("/assets/js/bootstrap.min.js"),array("jquery"),time(),true);
+        wp_enqueue_script("waypoints-min-js",get_theme_file_uri("/assets/js/jquery.waypoints.min.js"),array("jquery"),time(),true);
+        wp_enqueue_script("simplyCountdown-js",get_theme_file_uri("/assets/js/simplyCountdown.js"),array("jquery"),time(),true);
+        wp_enqueue_script("main-js",get_theme_file_uri("/assets/js/main.js"),array("jquery"),time(),true);
+  
+        $launcher_year = get_post_meta(get_the_ID(),"year",true);
+        $launcher_month = get_post_meta(get_the_ID(),"month",true);
+        $launcher_day = get_post_meta(get_the_ID(),"day",true);
+  
+        wp_localize_script("main-js","datedata",array(
+          "year" => $launcher_year,
+          "month" => $launcher_month,
+          "day" => $launcher_day
+        ));  
+    }else{
+        wp_enqueue_style("bootstrap-css",get_theme_file_uri("/assets/css/bootstrap.css"));
+        wp_enqueue_style("launcher", get_stylesheet_uri());
+        }
+    }
+      
   }
   add_action("wp_enqueue_scripts","launcher_assets");
   
---
+++ testtemplate.php
@@ -0,0 +1,16 @@
+<?php 
+/*
+** Template Name: Test Template
+*/
+?>
+<?php get_header();?>
+<html>
+    <Head>
+        
+    </head>
+    <body <?php body_class(); ?>>
+       <h1>
+           <?php the_title(); ?>
+       </h1> 
+    </body>
+</html>
